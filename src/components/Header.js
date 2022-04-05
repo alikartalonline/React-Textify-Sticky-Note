d387e16fc57bd06d3a2e37bced8d0614256ee660
Ali Kartal <ali_kartal91@hotmail.com>
2022-04-05 20:48:31 +0300

    "header added"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,33 @@
 import React from 'react'
 
+
 function Header() {
 
   return (
-    <div className='Header'>
+    <div >
+        <header className='Header'>
+            <div className='alikartal'>
+                
+                <h3 className='textify  '>
+                Press: "X"
+                </h3>
 
-    asfa
+                <a 
+                href="https://github.com/alikartalonline"
+                target="_blank"
+                >
+                    <img 
+                    className='logo'
+                    src="https://i.hizliresim.com/17vapvc.jpg"
+                    title="Ali Kartal" 
+                    alt="Ali Kartal" />
+                </a>
+                
+            </div>   
+        </header>
     </div>
   )
 }
 
 export default Header
+
